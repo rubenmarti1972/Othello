b4eb499d0612eac1a42a7af2b6038f66621ff6af
Ruben Martinez <lucasmartinez220372@gmail.com>
2025-06-12 03:28:06 -0500

    Ajustes en deploy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'adara-style' title`, () => {
+  it(`should have the 'othello' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('adara-style');
+    expect(app.title).toEqual('othello');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, adara-style');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, othello');
   });
 });
